ddc5fa6245c0c0cb256ae192a06a1480ef638a41
Itay Oshri <itay.oshri@gmail.com>
2025-07-26 23:25:23 +0300

    FEATURE: route

--- app/app/api/getPoints/route.ts
+++ app/app/api/getPoints/route.ts
@@ -1,4 +1,8 @@
 import { HandleUser } from "@/utils/account";
+import GetQuestionnaire from "@/utils/content/expandedContent";
+import GetAnswerId from "@/utils/content/questionnaire";
+import answerQuestion from "@/utils/content/questionnaire";
+import RecordLog from "@/utils/content/recordLog";
 
 type reqData = {
   verificationCode: string;
@@ -9,8 +13,17 @@ export async function POST(request: Request) {
   const data = (await request.json()) as reqData;
   const { mobilePhone, verificationCode } = data;
 
-  const accessToken = HandleUser(mobilePhone, verificationCode);
+  const accessToken = await HandleUser(mobilePhone, verificationCode);
 
+  /*
+  const questionnaire = await GetQuestionnaire(5384, accessToken);
+  console.log(questionnaire);
+  const questions = questionnaire.body.content.questions;
+  for (const question of questions) {
+    answerQuestion(await GetAnswerId(question.id));
+  }
+  await RecordLog(questionnaire.body.contentId, accessToken);
+  */
   return new Response(JSON.stringify(null), {
     headers: { "Content-Type": "application/json" },
   });
