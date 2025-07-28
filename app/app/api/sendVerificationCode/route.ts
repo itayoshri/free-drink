a9d3038515ac4b3f584e09bcc89f1777b43c1533
Itay Oshri <itay.oshri@gmail.com>
2025-07-28 22:11:35 +0300

    LOG

--- app/app/api/sendVerificationCode/route.ts
+++ app/app/api/sendVerificationCode/route.ts
@@ -3,10 +3,11 @@ import { NextRequest } from "next/server";
 
 export function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
+  console.log(searchParams);
   const mobilePhone = searchParams.get("mobilePhone");
 
   return new Response(
-    JSON.stringify(sendVerificationCode(mobilePhone as string)),
+    JSON.stringify(console.log(sendVerificationCode(mobilePhone as string))),
     {
       headers: { "Content-Type": "application/json" },
     }
