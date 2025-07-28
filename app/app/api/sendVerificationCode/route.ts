65366da92bf82e71bdbada72fdde29d804953cde
Itay Oshri <itay.oshri@gmail.com>
2025-07-28 22:14:20 +0300

    await

--- app/app/api/sendVerificationCode/route.ts
+++ app/app/api/sendVerificationCode/route.ts
@@ -1,13 +1,15 @@
 import sendVerificationCode from "@/utils/account/verificationCode";
 import { NextRequest } from "next/server";
 
-export function GET(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   console.log(searchParams);
   const mobilePhone = searchParams.get("mobilePhone");
 
   return new Response(
-    JSON.stringify(console.log(sendVerificationCode(mobilePhone as string))),
+    JSON.stringify(
+      console.log(await sendVerificationCode(mobilePhone as string))
+    ),
     {
       headers: { "Content-Type": "application/json" },
     }
