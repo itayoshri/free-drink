54b0ad4cea0ae42d7693b1cb049ab55937f375a8
Itay Oshri <itay.oshri@gmail.com>
2025-08-02 23:23:51 +0300

    FIX: Text sizing

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -15,7 +15,7 @@ export default function Input({
     <input
       onChange={(newValue) => onChange(newValue)}
       placeholder={placeholder}
-      className="text-black border-[1px] border-gray-300 w-full text-2xl font-medium outline-red-600 py-3 px-4 rounded-xl"
+      className="text-black border-[1px] border-gray-300 w-full text-xl font-medium outline-red-600 py-3 px-4 rounded-xl"
     />
   );
 }
