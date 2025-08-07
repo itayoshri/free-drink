5abd1d3a5f742dfec0c28022f3bac84855656eb9
Itay Oshri <itay.oshri@gmail.com>
2025-08-07 14:58:10 +0300

    FEATURE: Confetti after getting corks

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2,6 +2,7 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import Digit from "@/components/OTP/digit";
+import ScreenConfetti from "@/components/UI/Confetti";
 import axios from "axios";
 import { useCallback, useState } from "react";
 
@@ -9,6 +10,7 @@ export default function Home() {
   const [mobilePhone, setMobilePhone] = useState("0");
   const [showCode, setShowCode] = useState(false);
   const [digits, setDigits] = useState([0, 0, 0, 0]);
+  const [showConfetti, setConfetti] = useState(false);
   const changeDigit = useCallback((index: number, digit: number) => {
     setDigits((arr) => {
       arr[index] = digit;
@@ -33,6 +35,7 @@ export default function Home() {
       })
       .then((res) => {
         console.log(res.data);
+        setConfetti(true);
       });
   }, [digits, mobilePhone]);
 
@@ -40,23 +43,26 @@ export default function Home() {
     <div className="bg-white flex flex-col gap-4 justify-center items-center h-screen w-screen px-6">
       {/*TODO: seperate to components*/}
       {showCode ? (
-        <div className="flex flex-col items-center gap-9 w-full">
-          <h2 className="text-black text-xl font-bold">
-            הקלידו את קוד האימות שקיבלתם
-          </h2>
-          <div className="flex flex-col items-center w-full gap-4">
-            <div className="flex w-full gap-2 justify-between">
-              <Digit setValue={(digit) => changeDigit(3, digit)}></Digit>
-              <Digit setValue={(digit) => changeDigit(2, digit)}></Digit>
-              <Digit setValue={(digit) => changeDigit(1, digit)}></Digit>
-              <Digit setValue={(digit) => changeDigit(0, digit)}></Digit>
+        <>
+          <div className="flex flex-col items-center gap-9 w-full">
+            <h2 className="text-black text-xl font-bold">
+              הקלידו את קוד האימות שקיבלתם
+            </h2>
+            <div className="flex flex-col items-center w-full gap-4">
+              <div className="flex w-full gap-2 justify-between">
+                <Digit setValue={(digit) => changeDigit(3, digit)}></Digit>
+                <Digit setValue={(digit) => changeDigit(2, digit)}></Digit>
+                <Digit setValue={(digit) => changeDigit(1, digit)}></Digit>
+                <Digit setValue={(digit) => changeDigit(0, digit)}></Digit>
+              </div>
+              {/*<a className="text-black text-lg">לא קיבלתם את הקוד? שליחה מחדש</a>*/}
             </div>
-            {/*<a className="text-black text-lg">לא קיבלתם את הקוד? שליחה מחדש</a>*/}
+            <Button onClick={() => getPoints()} className="">
+              קבל 60 פקקים
+            </Button>
           </div>
-          <Button onClick={() => getPoints()} className="">
-            קבל 60 פקקים
-          </Button>
-        </div>
+          {showConfetti ? <ScreenConfetti /> : null}
+        </>
       ) : (
         <div className="flex flex-col w-full gap-8">
           <h1 className="text-5xl text-black font-bold">
