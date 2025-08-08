d416d50e4a858c5579d4fc968b62e1d46e36039d
Itay Oshri <itay.oshri@gmail.com>
2025-08-08 18:41:57 +0300

    FEATURE: Mobile phone input type

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -4,16 +4,19 @@ type InputProps = {
   onChange(newValue: ChangeEvent<HTMLInputElement>): unknown;
   placeholder: string;
   className?: string;
+  type?: string;
 };
 
 export default function Input({
   onChange,
   placeholder,
+  type = "text",
   className = "",
 }: InputProps) {
   return (
     <input
       onChange={(newValue) => onChange(newValue)}
+      type={type}
       placeholder={placeholder}
       className={`text-black border-[1px] border-gray-300 w-full text-xl font-medium outline-red-600 py-3 px-4 rounded-xl ${className}`}
     />
