223c3f1868a355a901a07691377967e223714007
itayoshri <itay.oshri@gmail.com>
2025-08-26 22:24:01 +0300

    First working answer from DB automation

--- app/app/api/getPoints/route.ts
+++ app/app/api/getPoints/route.ts
@@ -12,24 +12,26 @@ type reqData = {
 };
 
 export const fields = {
-  KnowledgeQuestionnaire: "",
+  KnowledgeQuestionnaire: "questionnaireId",
   Hotspots: "hotSpotQuestionnaireId",
 };
 
 export async function POST(request: Request) {
-  //const data = (await request.json()) as reqData;
-  //const { mobilePhone, verificationCode } = data;
+  const data = (await request.json()) as reqData;
+  const { mobilePhone, verificationCode } = data;
 
-  const accessToken =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjgzNTc3NCIsImh0dHA6Ly9taW50YXV0aC9jbGFpbXMvZ3VpZCI6IjIzZGZiNzVlLTYyMGItNGI0OS1hMmU4LWJkMzE2ODVhOTRmOCIsInJvbGUiOiIxIiwibmJmIjoxNzU2MTUyNDA0LCJleHAiOjE3NzE3MDQ0MDQsImlhdCI6MTc1NjE1MjQwNH0.2VCRY9RpRteD3YkIFhw8co1Mq3tU5NhyBd2mCVyTZ_Y"; //await HandleUser(mobilePhone, verificationCode);
+  const accessToken = await HandleUser(mobilePhone, verificationCode);
 
   const contents = (await GetHomePage(accessToken)).body.contents;
-  console.log(contents.length);
-  let counter = 0;
-  for (const content of contents) {
-    counter++;
+
+  for (const content of contents.filter(
+    (c) => c.formatType == "KnowledgeQuestionnaire"
+  )) {
     const { formatType: type, id } = content;
+    await RecordLog(id, "Started", accessToken);
+
     const questionnaire = await GetQuestionnaire(id, type, accessToken);
+
     try {
       //TODO: fix messy types
       const answers = await GetAnswers(
@@ -37,17 +39,12 @@ export async function POST(request: Request) {
         questionnaire.body.content.id
       );
 
-      const numberOfAnswers =
-        questionnaire.body.content.numberOfAnswersToSuccess;
-
-      //console.log(questionnaire);
-
-      if (answers.length >= numberOfAnswers) {
-        await RecordLog(id, "Started", accessToken);
-
+      const numberOfQuestions = questionnaire.body.content.questions.length;
+      console.log(answers);
+      if (answers.length == numberOfQuestions) {
         for (const index in answers) {
           const answer = answers[index];
-          const isLastQuestion = numberOfAnswers == Number(index);
+          const isLastQuestion = numberOfQuestions == Number(index) + 1;
           console.log(
             await AnswerQuestion(
               answer,
@@ -62,7 +59,7 @@ export async function POST(request: Request) {
     } catch {}
   }
 
-  return new Response(JSON.stringify(counter), {
+  return new Response(JSON.stringify(0), {
     headers: { "Content-Type": "application/json" },
   });
 }
