45665d115efd7c8c960266f09a0a5292d00656e2
itayoshri <itay.oshri@gmail.com>
2025-08-30 12:19:35 +0300

    DB content interface

---
+++ app/interfaces/db/index.ts
@@ -0,0 +1,8 @@
+import { ReqQuestionData } from "../api/requests";
+import { ResExpendedContent } from "../api/res";
+
+export interface DBContent {
+  contentId: number;
+  type: string; // TODO: type
+  content: ResExpendedContent["body"]["content"];
+}
