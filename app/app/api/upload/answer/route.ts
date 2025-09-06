6d2d4cc8d17f431518ebf853e5e359e172921750
Itay Oshri <itay.oshri@gmail.com>
2025-09-06 15:23:08 +0300

    remove commented code

--- app/app/api/upload/answer/route.ts
+++ app/app/api/upload/answer/route.ts
@@ -9,25 +9,8 @@ type reqData = {
 export async function POST(request: Request) {
   const data = (await request.json()) as reqData;
   const { answer } = data;
-  console.log(answer);
-
-  //const accessToken = await HandleUser(mobilePhone, verificationCode);
-  //console.log(accessToken);
-
-  //const contents = (await GetHomePage(accessToken)).body.contents;
-  //UpdateContent(contents, accessToken);
   await AddAnswer(answer);
 
-  /*
-
-  const questionnaire = await GetQuestionnaire(6591, accessToken);
-  console.log(questionnaire);
-  const questions = questionnaire.body.content.questions;
-  for (const question of questions) {
-    AnswerQuestion(await GetAnswerId(question.id));
-  }
-  await RecordLog(questionnaire.body.contentId, "Finished", accessToken);
-  */
   return new Response(JSON.stringify(""), {
     headers: { "Content-Type": "application/json" },
   });
