8701aee4abe0ac2c8d8c0f5d3400c01807730c6b
Itay Oshri <itay.oshri@gmail.com>
2025-09-19 00:07:38 +0300

    FEATURE: show meesage about the action

--- app/app/Completed.tsx
+++ app/app/Completed.tsx
@@ -1,20 +1,24 @@
 "use client";
 import Button from "@/components/button";
-import RequestActionCard, { Action } from "@/components/UI/RequestActionCard";
+import RequestActionCard, {
+  Action,
+  actionInfo,
+} from "@/components/UI/RequestActionCard";
 import { useAuth } from "@/context";
 
-const SUCCEED_TEXT = "הפעולה הושלמה בהצלחה, קיבלתם 80 פקקים!";
-const FAILED_TEXT = "הפעם זה לא הצליח :(";
 export default function CompletedPage() {
   const { getPointsResData } = useAuth();
-  const action = getPointsResData["succses"]
+
+  // TODO: Get request duration
+  // TODO: Get in better way from req data
+  const action = getPointsResData.succses
     ? Action.TWO_QUESTIONS_OR_MORE
     : Action.FAILED;
 
   return (
     <div className="flex flex-col w-full gap-6 items-center">
       <a className="text-center font-bold text-3xl text-black">
-        הפעולה הושלמה בהצלחה, קיבלתם 80 פקקים!
+        {actionInfo[action].message}
       </a>
       <div className="flex flex-col w-full gap-4">
         <RequestActionCard action={action} duration={2} />
--- app/components/UI/RequestActionCard/index.tsx
+++ app/components/UI/RequestActionCard/index.tsx
@@ -11,16 +11,19 @@ type RequestAction = {
   duration: number;
 };
 
-const actionInfo = [
-  "לא נפתרו חידות",
-  "פתירת חידה אחת",
-  "פתירת שתי חידות או יותר",
+export const actionInfo = [
+  { action: "לא נפתרו חידות", message: "הפעם זה לא הצליח :(" },
+  { action: "פתירת חידה אחת", message: "כמעט הצלחנו, קיבלתם 70 פקקים!" },
+  {
+    action: "פתירת שתי חידות או יותר",
+    message: "הפעולה הושלמה בהצלחה, קיבלתם 80 פקקים!",
+  },
 ];
 
 export default function RequestActionCard({ action, duration }: RequestAction) {
   return (
     <div className="bg-white flex flex-col w-full border-[1px] border-gray-300 px-4 py-5 rounded-xl gap-4">
-      <InfoLine title="פעולה" data={actionInfo[action]} />
+      <InfoLine title="פעולה" data={actionInfo[action].action} />
       <InfoLine title="משך" data={`${duration} שניות`} />
     </div>
   );
