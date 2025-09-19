1710753f430cb4797ceb671ab57638b11ecd70c3
Itay Oshri <itay.oshri@gmail.com>
2025-09-20 00:02:27 +0300

    Remove loading page

--- app/app/loading/page.tsx
+++
@@ -1,7 +0,0 @@
-import CompletedPage from "@/app/Completed";
-import LoadingPage from "@/app/Loading";
-
-export default function Loading() {
-  const loading = false;
-  return loading ? <LoadingPage /> : <CompletedPage></CompletedPage>;
-}
