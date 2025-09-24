a559139f1b6a3c4e30617e125746b7601ee7d089
Itay Oshri <itay.oshri@gmail.com>
2025-09-24 23:43:41 +0300

    TEMPORARY fix for env accses

--- app/utils/db/index.ts
+++ app/utils/db/index.ts
@@ -1,11 +1,12 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 
-const db_username = process.env.DB_USERNAME;
-const db_password = process.env.DB_PASSWORD;
-
-const uri = `mongodb+srv://${db_username}:${db_password}@cluster0.4xnbwfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// TODO: fix env accses from solver
 
 export default function GetDB() {
+  const db_username = process.env.DB_USERNAME;
+  const db_password = process.env.DB_PASSWORD;
+  const uri = `mongodb+srv://${db_username}:${db_password}@cluster0.4xnbwfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
   const client = new MongoClient(uri, {
     serverApi: {
       version: ServerApiVersion.v1,
