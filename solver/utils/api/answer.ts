fa86b4e4942c00ea48555450276791b6af8a0712
Itay Oshri <itay.oshri@gmail.com>
2025-09-28 22:45:24 +0300

    FIX: change problematic symbols and chars before parsing

--- solver/utils/api/answer.ts
+++ solver/utils/api/answer.ts
@@ -6,10 +6,16 @@ export default function formatAnswerRequest(
   modelAnswer: string,
   data: FormattedQuestionnaire
 ) {
+  // TODO: turn into seperate function
   const firstBrace = modelAnswer.indexOf("{");
   const lastBrace = modelAnswer.lastIndexOf("}");
-  const jsonString = modelAnswer.slice(firstBrace, lastBrace + 1);
+  let jsonString = modelAnswer.slice(firstBrace, lastBrace + 1);
+  jsonString = jsonString.replace(/[“”„]/g, '"'); // double quotes
+  jsonString = jsonString.replace(/[‘’‚‛]/g, "'"); // single quotes
+  jsonString = jsonString.replace(/[\u0000-\u001F\u007F]/g, "");
+
   const JsonModelAnswer = JSON.parse(jsonString).result as ModelSchema;
+  console.log(JsonModelAnswer);
   const signedHash = GenerateSignedHash(JsonModelAnswer.questionId, data.id);
   return {
     questionId: JsonModelAnswer.questionId,
