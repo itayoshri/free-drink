d15c391c63a00caee214931e698dda48f57b7d5c
Itay Oshri <itay.oshri@gmail.com>
2025-10-05 17:38:11 +0300

    INIT: createUser

--- auth/src/users/users.service.ts
+++ auth/src/users/users.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -16,5 +17,22 @@ export class UsersService {
 
   findOne(phone_number: string): Promise<User | null> {
     return this.usersRepository.findOneBy({ phone_number });
+  }
+
+  async createUser(
+    phone_number: string,
+    role_key: string,
+    invitation_token?: string,
+  ) {
+    const password_hash = await bcrypt.hash('', 12);
+
+    const user = this.usersRepository.create({
+      phone_number,
+      role_key,
+      password_hash,
+      created_token_id: invitation_token,
+    });
+
+    await this.usersRepository.save(user);
   }
 }
