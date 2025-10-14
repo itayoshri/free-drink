36b034f9a50166d8c7e5a733a4f3808fb711bf74
Itay Oshri <itay.oshri@gmail.com>
2025-10-14 22:36:02 +0300

    INIT: check if user exissts front

--- app/app/account/Form.tsx
+++ app/app/account/Form.tsx
@@ -1,12 +1,20 @@
 "use client";
 import Input from "@/components/input";
 import PhoneNumberInput from "@/components/UI/PhoneNumberInput";
-import { useState } from "react";
+import axios from "axios";
+import { useCallback, useState } from "react";
 
 const TITLE = "התחברו או צרו משתמש";
 const SUBTITLE = "עם משתמש רשום ניתן לקבל 80 פקיים ויותר";
 export default function AccountForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
+  const checkIfUserExists = useCallback((phoneNumber: string) => {
+    axios.get(`${URL}/users/exists?phoneNumber=${phoneNumber}`).then((res) => {
+      if (res.status == 200) {
+        console.log(res.data);
+      }
+    });
+  }, []);
   return (
     <div className="flex flex-col gap-4 w-full">
       <div className="flex flex-col text-black items-center gap-2">
@@ -14,8 +22,8 @@ export default function AccountForm() {
         <h4>{SUBTITLE}</h4>
       </div>
       <PhoneNumberInput
-        onChange={(newValue) => setPhoneNumber("")}
-        onClick={() => null}
+        onChange={(newValue) => setPhoneNumber(newValue.currentTarget.value)}
+        onClick={() => checkIfUserExists(phoneNumber)}
         placeholder="מספר טלפון"
       >
         המשיכו
