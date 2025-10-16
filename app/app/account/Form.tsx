7f39ba900c5f28df275b28ef06fc11bebf940d80
Itay Oshri <itay.oshri@gmail.com>
2025-10-17 01:13:47 +0300

    Transfer people to different pages based on if they have account

--- app/app/account/Form.tsx
+++ app/app/account/Form.tsx
@@ -2,19 +2,31 @@
 import Input from "@/components/input";
 import PhoneNumberInput from "@/components/UI/PhoneNumberInput";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 
 const TITLE = "התחברו או צרו משתמש";
 const SUBTITLE = "עם משתמש רשום ניתן לקבל 80 פקיים ויותר";
 export default function AccountForm() {
   const [phoneNumber, setPhoneNumber] = useState("");
-  const checkIfUserExists = useCallback((phoneNumber: string) => {
-    axios.get(`${URL}/users/exists?phoneNumber=${phoneNumber}`).then((res) => {
-      if (res.status == 200) {
-        console.log(res.data);
-      }
-    });
-  }, []);
+  const router = useRouter();
+
+  const checkIfUserExists = useCallback(
+    (phoneNumber: string) => {
+      axios
+        .get(
+          `${process.env.NEXT_PUBLIC_AUTH_SERVER_URL}/users/exists?phoneNumber=${phoneNumber}`
+        )
+        .then((res) => {
+          if (res.status == 200) {
+            if (res.data.data.exists) {
+              router.push("/account/login");
+            } else router.push("/account/register");
+          }
+        });
+    },
+    [router]
+  );
   return (
     <div className="flex flex-col gap-4 w-full">
       <div className="flex flex-col text-black items-center gap-2">
--- auth/src/main.ts
+++ auth/src/main.ts
@@ -5,6 +5,10 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3002);
 }
