fd0dd91625b722e2e1a89e82cb3eb211c99772bd
Itay Oshri <itay.oshri@gmail.com>
2025-10-18 17:22:02 +0300

    INIT: login page

--- app/app/account/login/page.tsx
+++ app/app/account/login/page.tsx
@@ -1,11 +1,49 @@
+"use client";
+import Button from "@/components/button";
 import Input from "@/components/input";
+import { useSearchParams } from "next/navigation";
+import { AUTH_SERVER_URL } from "../Form";
+import axios from "axios";
+import React from "react";
 
-export default function LoginPage({
-  searchParams,
-}: {
-  searchParams?: { [key: string]: string | string[] | undefined };
-}) {
-  const phoneNumber = searchParams?.phoneNumber || "";
+export default function LoginPage() {
+  const searchParams = useSearchParams();
+  const phoneNumber = searchParams.get("phoneNumber") || "";
 
-  return <div></div>;
+  const onSubmit = async (phoneNumber: string, password: string) => {
+    try {
+      const res = await axios.post(
+        `${AUTH_SERVER_URL}/auth/login`,
+        {
+          phoneNumber,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      console.log(res.data);
+    } catch (error) {}
+  };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const phoneNumber = formData.get("phone") as string;
+    const password = formData.get("password") as string;
+    onSubmit(phoneNumber, password);
+  };
+
+  return (
+    <div className="flex flex-col w-full">
+      <form className="flex flex-col w-full gap-form" onSubmit={handleSubmit}>
+        <Input
+          name="phone"
+          placeholder="מספר טלפון"
+          type="tel"
+          defaultValue={phoneNumber}
+        />
+        <Input name="password" placeholder="סיסמה" type="password" />
+        <Button type="submit">התחברו</Button>
+      </form>
+    </div>
+  );
 }
