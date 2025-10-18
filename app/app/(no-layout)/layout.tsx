71d4829ada9c332521b1a4abcaceb41603e7e7ba
Itay Oshri <itay.oshri@gmail.com>
2025-10-18 16:39:18 +0300

    REFACTOR: remove unnecrey parent

--- app/app/(no-layout)/layout.tsx
+++ app/app/(no-layout)/layout.tsx
@@ -7,18 +7,14 @@ import LoadingPage from "../Loading";
 export default function NoLayout({ children }: { children: React.ReactNode }) {
   const { loading } = useAuth();
 
-  return (
+  return loading ? (
+    <LoadingPage />
+  ) : (
     <>
-      {loading ? (
-        <LoadingPage />
-      ) : (
-        <>
-          <Navbar />
-          <main className="flex-1 flex flex-col justify-center items-center px-6">
-            {children}
-          </main>
-        </>
-      )}
+      <Navbar />
+      <main className="flex-1 flex flex-col justify-center items-center px-6">
+        {children}
+      </main>
     </>
   );
 }
