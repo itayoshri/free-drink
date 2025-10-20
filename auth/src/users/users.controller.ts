1c0b76954d910addf74972c56c4d2c9f8ee37505
Itay Oshri <itay.oshri@gmail.com>
2025-10-20 14:14:09 +0300

    FEATURE: sent auth cookies back to user after account creation request

--- auth/src/users/users.controller.ts
+++ auth/src/users/users.controller.ts
@@ -1,19 +1,37 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { AuthService } from 'src/auth/auth.service';
+import type { Response } from 'express';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    readonly authService: AuthService,
+  ) {}
 
   @Post('create')
-  async CreateUser(@Body() data: CreateUserDto) {
+  async CreateUser(
+    @Body() data: CreateUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const { phoneNumber, password, invitationToken } = data;
     const user = await this.usersService.createUser(
       phoneNumber,
       password,
       invitationToken,
     );
+
+    const {
+      accessToken,
+      expiresAccessToken,
+      refreshToken,
+      expiresRefreshToken,
+    } = await this.authService.generateTokens(user);
+
+    this.authService.setAuthCookie(res, accessToken, expiresAccessToken);
+    this.authService.setRefreshCookie(res, refreshToken, expiresRefreshToken);
 
     return {
       message: 'user has been created',
--- auth/src/users/users.module.ts
+++ auth/src/users/users.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersController } from './users.controller';
 import { InvitationModule } from 'src/invitation/invitation.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
     forwardRef(() => InvitationModule),
+    forwardRef(() => AuthModule),
     TypeOrmModule.forFeature([User]),
   ],
   providers: [UsersService],
