291995c1833f7dc532117709213def59dd397ec8
Itay Oshri <itay.oshri@gmail.com>
2025-10-22 22:29:57 +0300

    FEATURE: Logout popup

--- app/components/Icons/index.tsx
+++ app/components/Icons/index.tsx
@@ -23,3 +23,13 @@ export const SupervisorAccount = createSVGResource(
   <path d="M0 0h24v24H0z" fill="none" />,
   <path d="M16.5 12c1.38 0 2.49-1.12 2.49-2.5S17.88 7 16.5 7C15.12 7 14 8.12 14 9.5s1.12 2.5 2.5 2.5zM9 11c1.66 0 2.99-1.34 2.99-3S10.66 5 9 5C7.34 5 6 6.34 6 8s1.34 3 3 3zm7.5 3c-1.83 0-5.5.92-5.5 2.75V19h11v-2.25c0-1.83-3.67-2.75-5.5-2.75zM9 13c-2.33 0-7 1.17-7 3.5V19h7v-2.25c0-.85.33-2.34 2.37-3.47C10.5 13.1 9.66 13 9 13z" />
 );
+
+export const Logout = createSVGResource(
+  <path d="M0,0h24v24H0V0z" fill="none" />,
+  <path d="M17,8l-1.41,1.41L17.17,11H9v2h8.17l-1.58,1.58L17,16l4-4L17,8z M5,5h7V3H5C3.9,3,3,3.9,3,5v14c0,1.1,0.9,2,2,2h7v-2H5V5z" />
+);
+
+export const Add = createSVGResource(
+  <path d="M0 0h24v24H0V0z" fill="none" />,
+  <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+);
--- app/components/UI/Navbar/Popup.tsx
+++ app/components/UI/Navbar/Popup.tsx
@@ -1,13 +1,12 @@
 "use client";
-import Button from "@/components/button";
-import { Account } from "@/components/Icons";
-import { Icon } from "@/components/Icons/svgFactory";
-import { User } from "@/context/auth";
+import { Account, Logout } from "@/components/Icons";
+import { useAuth, User } from "@/context/auth";
 import { ReactElement } from "react";
 
 export default function AccountPopup({ user }: { user: User }) {
+  const { logout } = useAuth();
   return (
-    <div className="flex flex-col gap-2 absolute top-full h-fit bg-white rounded-xl border-[1px] p-4 border-gray-300 pl-20">
+    <div className="flex flex-col gap-4 absolute top-full h-fit bg-white rounded-xl border-[1px] p-4 border-gray-300 pl-20">
       <div className="flex items-center gap-2">
         <Account className="text-black" height={35} />
         <div className="flex flex-col font-inter">
@@ -16,8 +15,13 @@ export default function AccountPopup({ user }: { user: User }) {
         </div>
       </div>
       <div className="flex flex-col">
-        <PopupButton text="הזנת קוד הצטרפות" icon={<Account width={20} />} />
-        <PopupButton text="התנתקות" warning icon={<Account width={20} />} />
+        {/*<PopupButton text="הזנת קוד הצטרפות" icon={<Add width={20} />} />*/}
+        <PopupButton
+          text="התנתקות"
+          warning
+          icon={<Logout width={20} />}
+          onClick={() => logout()}
+        />
       </div>
     </div>
   );
