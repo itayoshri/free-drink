2197a54657bbed2f5c708b237f47acd7aed5d825
Itay Oshri <itay.oshri@gmail.com>
2025-10-23 23:43:06 +0300

    FEATURE: Refill menu

--- app/app/refill/page.tsx
+++ app/app/refill/page.tsx
@@ -1,12 +1,24 @@
 import { Add, Wind } from "@/components/Icons";
 import MenuItem from "@/components/refill/MenuItem";
+import Logo from "@/components/UI/Logo";
+
+const BASE_PATH = "refill";
 
 export default function RefillMenu() {
   return (
-    <div className="flex flex-col w-full gap-form">
+    <div className="flex flex-col w-full gap-8">
+      <Logo version="refill" className="h-6 fill-primary-500" />
       <div className="flex flex-col w-full gap-form">
-        <MenuItem text="מחולל קודי הזמנה" icon={<Add width={25} />} />
-        <MenuItem text="פותר חידות" icon={<Wind width={25} />} />
+        <MenuItem
+          text="מחולל קודי הזמנה"
+          icon={<Add width={25} />}
+          page={`${BASE_PATH}/invitation`}
+        />
+        <MenuItem
+          text="פותר חידות"
+          icon={<Wind width={25} />}
+          page={`${BASE_PATH}/solver`}
+        />
       </div>
     </div>
   );
--- app/components/refill/MenuItem.tsx
+++ app/components/refill/MenuItem.tsx
@@ -1,20 +1,21 @@
+import Link from "next/link";
 import { ReactElement } from "react";
 
 type MenuItemProps = {
   text: string;
   icon: ReactElement;
-  onClick?(): unknown;
+  page: string;
 };
 
-export default function MenuItem({ text, icon, onClick }: MenuItemProps) {
+export default function MenuItem({ text, icon, page }: MenuItemProps) {
   return (
-    <button
-      onClick={onClick}
+    <Link
+      href={page}
       className="flex flex-col w-full items-start gap-form px-page py-6 rounde font-bold rounded-3xl text-white border-zinc-700 border-[1px]"
     >
       {icon}
 
       {text}
-    </button>
+    </Link>
   );
 }
