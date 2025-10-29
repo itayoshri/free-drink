9d95f961e8c4916a8cd9ec904bb0c26554946114
Itay Oshri <itay.oshri@gmail.com>
2025-10-29 22:16:00 +0200

    FIX: return to first step if on get points

--- app/components/UI/Navbar/index.tsx
+++ app/components/UI/Navbar/index.tsx
@@ -4,10 +4,21 @@ import Logo from "../Logo";
 import SignedUser from "./SignedUser";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+import { useCallback } from "react";
+import { usePathname } from "next/navigation";
+import { useApp } from "@/context";
 
 export default function Navbar() {
   const { isAuth, user } = useAuth();
+  const { setStep } = useApp();
+
   const router = useRouter();
+  const pathname = usePathname();
+  const navigateHome = useCallback(() => {
+    if (pathname === "/") setStep("phoneNumber");
+    else router.push("/");
+  }, [pathname, router, setStep]);
+
   return (
     <div className="flex sticky border-b-[1px] border-gray-300 dark:border-zinc-800 bg-white dark:bg-zinc-900 text-black dark:text-white justify-between h-14 items-center w-full px-page py-3 top-0">
       {isAuth ? (
@@ -20,9 +31,9 @@ export default function Navbar() {
           כניסה
         </button>
       )}
-      <Link href={"/"}>
+      <button onClick={() => navigateHome()}>
         <Logo className="h-4 fill-primary" version="short" />
-      </Link>
+      </button>
     </div>
   );
 }
