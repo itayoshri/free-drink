f9ba7f58cb16f168dd7ca8370d432ddd4e361095
Itay Oshri <itay.oshri@gmail.com>
2025-10-31 18:29:52 +0200

    FIX: Show Admin button only to admins

--- app/components/UI/Navbar/SignedUser.tsx
+++ app/components/UI/Navbar/SignedUser.tsx
@@ -4,17 +4,28 @@ import AccountPopup from "./Popup";
 import { useState } from "react";
 import Link from "next/link";
 import { User } from "@/interfaces/db/auth";
+import hasPermission from "@/utils/auth/permissions";
+import { useAuth } from "@/context/auth";
 
 export default function SignedUser({ user }: { user: User }) {
   const [isPopupOpened, setPopupOpened] = useState(false);
+  const { rolesMap } = useAuth();
+  const canAccessRefill = hasPermission(
+    user as User,
+    "invitation.generate",
+    rolesMap
+  );
+
   return (
     <div className="flex gap-3">
       <button onClick={() => setPopupOpened((current) => !current)}>
         <Account width={28} />
       </button>
-      <Link href={"/refill"}>
-        <SupervisorAccount width={28} />
-      </Link>
+      {canAccessRefill && (
+        <Link href={"/refill"}>
+          <SupervisorAccount width={28} />
+        </Link>
+      )}
       {isPopupOpened ? <AccountPopup user={user} /> : null}
     </div>
   );
