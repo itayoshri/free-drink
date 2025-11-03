6c6c8e7609123cee5e8e8192cdc27faf986cc4c8
itayoshri <itay.oshri@gmail.com>
2025-11-03 07:55:43 +0200

    FIX: use readonly instead of disabled in Input component

--- app/components/input.tsx
+++ app/components/input.tsx
@@ -25,7 +25,7 @@ export default function Input({
       name={name}
       placeholder={placeholder}
       defaultValue={defaultValue}
-      disabled={disabled}
+      readOnly={disabled}
       className={`text-black border-[1px] border-gray-300 w-full text-xl font-medium outline-primary py-3 px-4 rounded-xl ${className} ${
         leftToRight ? "ltr" : ""
       } ${disabled && "opacity-60 bg-gray-200"}`}
