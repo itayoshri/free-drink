dff44e85c80457d7470a14f278f4bc20b30f5376
Itay Oshri <itay.oshri@gmail.com>
2025-11-18 15:42:52 +0200

    fix: dev

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'http://192.168.0.116:3000',
+      'http://192.168.0.163:3000',
       'http://127.0.0.1:3000',
     ],
     credentials: true,
