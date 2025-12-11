1a9f495dcbac95478b87a9337479a4a3800f2cc5
Itay Oshri <itay.oshri@gmail.com>
2025-12-11 14:01:27 +0200

    REFACTOR: Gett roles from cache instead of fetch

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -3,11 +3,12 @@ import "./globals.css";
 import { AppProvider } from "@/context";
 import { AuthProvider } from "@/context/auth";
 import { cookies } from "next/headers";
-import axios from "axios";
-import { getUserPermissions } from "@/utils/auth/permissions";
+import {
+  getAppPermissions,
+  getUserPermissions,
+} from "@/utils/auth/permissions";
 import { UserProvider } from "@/context/user";
 import { LoadingProvider } from "@/context/loading";
-const AUTH_SERVER_URL = process.env.NEXT_PUBLIC_AUTH_SERVER_URL || "";
 
 export const metadata: Metadata = {
   title: "Free Drink",
@@ -21,8 +22,7 @@ export default async function RootLayout({
   const cookieStore = await cookies();
   const accessToken = cookieStore.get("access_token");
   const isAuth = Boolean(accessToken?.value);
-  const rolesMap = (await axios.get(`${AUTH_SERVER_URL}/invitation/roles`)).data
-    .data.roles;
+  const rolesMap = await getAppPermissions();
   const { role: role_key, phone_number } = getUserPermissions(
     accessToken?.value as string
   );
