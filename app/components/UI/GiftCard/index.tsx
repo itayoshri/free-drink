960de86b9b848b02c7733955eba0c6bc5d0dc7bb
Itay Oshri <itay.oshri@gmail.com>
2025-12-30 15:36:01 +0200

    FEATURE: GifCard component

---
+++ app/components/UI/GiftCard/index.tsx
@@ -0,0 +1,23 @@
+import Logo from "../Logo";
+import GiftCardVector from "./vector";
+
+const TOP_TEXT = "שובר למשקה ממכונת שמקר";
+
+type GiftCardProps = {
+  cardId: number;
+};
+
+export default function GiftCard({ cardId }: GiftCardProps) {
+  return (
+    <div className="relative text-white w-64 h-64 overflow-hidden flex flex-col items-center justify-center">
+      <div className="flex flex-col gap-3 justify-center items-center h-full z-10">
+        <Logo className="h-3 fill-white" />
+        <a>{TOP_TEXT}</a>
+      </div>
+      <div className="flex justify-center items-center h-full z-10 font-code font-bold">
+        <a>{cardId}</a>
+      </div>
+      <GiftCardVector className="absolute inset-0 w-full pointer-events-none" />
+    </div>
+  );
+}
