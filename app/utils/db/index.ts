818c28d2789061925b55d17aa6ad8eb8444fc46c
Itay Oshri <itay.oshri@gmail.com>
2026-04-15 22:30:42 +0300

    REFACTOR: Migrate db to another region because of avilavility

--- app/utils/db/index.ts
+++ app/utils/db/index.ts
@@ -3,7 +3,7 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 const db_username = process.env.DB_USERNAME;
 const db_password = process.env.DB_PASSWORD;
 
-const uri = `mongodb+srv://${db_username}:${db_password}@cluster0.4xnbwfp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${db_username}:${db_password}@cluster0.8lfx6wm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 export default function GetDB() {
   const client = new MongoClient(uri, {
